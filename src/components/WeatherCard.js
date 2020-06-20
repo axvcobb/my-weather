cb551453b6c5e04b4a683c68a169457229583fa9
Alex Cobb <axvcobb@gmail.com>
2020-06-19 23:22:13 -0400

    More weathercard updates.  Going to focus on the CurrentCard from now on

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 class WeatherCard extends React.Component {
 
-  formatDate(date) {
+  formatDate(unix) {
+    var date = new Date(unix);
     var year = date.getYear();
     var month = (1 + date.getMonth()).toString();
     var day = date.getDate().toString();
@@ -12,17 +13,17 @@ class WeatherCard extends React.Component {
   }
 
   render() {
-    var date = new Date(this.props.data.dt * 1000);
     return (
       <div className="card">
         <div className="content">
           <div className="header" style={{ textAlign: 'center'}}>{this.props.data.temp.min}&deg; - {this.props.data.temp.max}&deg;</div>
-          <div className="meta" style={{ textAlign: 'center'}}>{this.formatDate(date)}</div>
+          <div className="meta" style={{ textAlign: 'center'}}>{this.formatDate(this.props.data.dt * 1000)}</div>
           <div className="description">
             <ul>
+              <li>Weather: {this.props.data.weather[0].description}</li>
               <li>Morning: {this.props.data.feels_like.morn}&deg; </li>
               <li>Evening: {this.props.data.feels_like.eve}&deg; </li>
-              <li>Weather: {this.props.data.weather[0].description}</li>
+              <li>Sunset: {}</li>
             </ul>
           </div>
         </div>
