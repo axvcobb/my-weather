b0b1c655c1f590f2f1b8337969234c78eee2129f
Alex Cobb <axvcobb@gmail.com>
2020-07-11 14:56:33 -0400

    Working icons, added formatTime() method for sunset math

--- src/components/CurrentCard.js
+++ src/components/CurrentCard.js
@@ -4,7 +4,7 @@ class CurrentCard extends React.Component {
 
   formatDate(unix) {
     var date = new Date(unix);
-    var year = date.getYear();
+    var year = date.getYear() - 100;
     var month = (1 + date.getMonth()).toString();
     var day = date.getDate().toString();
     var dayOfWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"][ date.getDay() ];
@@ -12,17 +12,30 @@ class CurrentCard extends React.Component {
     return dayOfWeek + ' - ' + month + '/' + day + '/' + year;
   }
 
+  formatTime(unix) {
+
+    var date = new Date(unix);
+    var hours = date.getHours() > 12 ? date.getHours() - 12 : date.getHours();
+    var minutes = "0" + date.getMinutes();
+    var seconds = "0" + date.getSeconds();
+
+    return hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+  }
+
 
   render() {
     console.log(this.props.daily);
+    const iconSrc = 'http://openweathermap.org/img/wn/' + this.props.current.weather[0].icon + '.png';
     return (
       <div className="card">
         <div className="content">
+          <div className="header" style={{ textAlign: 'center'}}><img src={iconSrc} alt={this.props.current.weather[0].main} /></div>
           <div className="header" style={{ textAlign: 'center'}}>{this.props.current.temp}&deg;</div>
           <div className="meta" style={{ textAlign: 'center'}}>{this.formatDate(this.props.current.dt * 1000)}</div>
           <div className="description">
             <ul>
-              <li>Feels Like: {this.props.current.feels_like}&deg;</li>
+              <li><strong>Feels Like: </strong>{this.props.current.feels_like}&deg;</li>
+              <li><strong>Sunset: </strong>{this.formatTime(this.props.current.sunset * 1000)}</li>
             </ul>
           </div>
         </div>
--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -12,10 +12,21 @@ class WeatherCard extends React.Component {
     return dayOfWeek + ' - ' + month + '/' + day + '/' + year;
   }
 
+  formatTime(unix) {
+    var date = new Date(unix);
+    var hours = date.getHours() > 12 ? date.getHours() - 12 : date.getHours();
+    var minutes = "0" + date.getMinutes();
+    var seconds = "0" + date.getSeconds();
+
+    return hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+  }
+
   render() {
+    const iconSrc = 'http://openweathermap.org/img/wn/' + this.props.data.weather[0].icon + '.png';
     return (
       <div className="card">
         <div className="content">
+          <div className="header" style={{ textAlign: 'center'}}><img src={iconSrc} alt={this.props.data.weather[0].main} /></div>
           <div className="header" style={{ textAlign: 'center'}}>{this.props.data.temp.min}&deg; - {this.props.data.temp.max}&deg;</div>
           <div className="meta" style={{ textAlign: 'center'}}>{this.formatDate(this.props.data.dt * 1000)}</div>
           <div className="description">
@@ -23,7 +34,7 @@ class WeatherCard extends React.Component {
               <li>Weather: {this.props.data.weather[0].description}</li>
               <li>Morning: {this.props.data.feels_like.morn}&deg; </li>
               <li>Evening: {this.props.data.feels_like.eve}&deg; </li>
-              <li>Sunset: {}</li>
+              <li>Sunset: {this.formatTime(this.props.data.sunset * 1000 )}</li>
             </ul>
           </div>
         </div>
